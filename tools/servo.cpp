381e92eab254364d91e6b64ac55db57fc2dfc304
bork <bork.andrew.ryan@gmail.com>
2022-05-19 20:49:24 -0700

    fasdfdsfadsdsf

--- tools/servo.cpp
+++ tools/servo.cpp
@@ -5,21 +5,34 @@
 
 int main(int argc, char ** argv){
 
-    int mode = 0;
+    char mode = ' ';
     double percent = 0.5;
     int ms = 0;
+    int pin = 0;
     if(argc >= 2){
-        mode = atoi(argv[1]);
-        if(mode == 0){
-            if(argc >= 3){
-                ms = atoi(argv[2]);
-            }
-        }else{
-            if(argc >= 3){
-                percent = atof(argv[2]);
-            }
+        pin = atoi(argv[1]);
+        printf("DIDNT SPECIFY PIN (0-15)\n");
+    }
+    if(argc >= 3){
+        mode = argv[2][0];
+        printf("DIDNT SPECIFY MODE (t/p)\n");
+    }
+    if(argc == 4){
+        if(mode == 't'){
+            ms = atoi(argv[3]);
+        }else if(mode == 'p'){
+            percent = atof(argv[3]);
         }
     }
+
+    printf("\"servo\" program:\n");
+    printf("rotating servo at pin \"%d\"\n", pin)
+    if(mode == 't'){
+        printf("mode \"time\": setting on time to %d ms.\n", ms);
+    }else if(mode == 'p'){
+        printf("mode \"percent\": setting to %f%% rotation.\n", ((int) percent*1000) / 10.0);
+    }
+
 
     pca9685::init();
     pca9685::set_frequency(50);
@@ -30,10 +43,9 @@ int main(int argc, char ** argv){
     printf("Mode 1 reg: %d\n", pca9685::query_reg(MODE_1) & 0xFF);
     // pca9685::set_pwm_off(15, 205);
     // pca9685::set_pwm_on(15, 0);
-    if(mode == 0){
+    if(mode == 't'){
         pca9685::set_pwm_ms(15, ms);
-    }else{
-
+    }else if(mode == 'p'){
         pca9685::set_pwm_percent(15, percent);
     }
     
