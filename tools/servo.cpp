c349a59b06358ca806f7af7960be7d9d7b330012
bork <bork.andrew.ryan@gmail.com>
2022-05-19 21:06:44 -0700

    fdsadfsdasf

--- tools/servo.cpp
+++ tools/servo.cpp
@@ -48,14 +48,14 @@ int main(int argc, char ** argv){
     // pca9685::set_pwm_off(15, 205);
     // pca9685::set_pwm_on(15, 0);
     if(mode == 't'){
-        pca9685::set_pwm_ms(15, ms);
+        pca9685::set_pwm_ms(pin, ms);
     }else if(mode == 'p'){
-        pca9685::set_pwm_percent(15, percent);
+        pca9685::set_pwm_percent(pin, percent);
     }
     
     usleep(500);
-    printf("OFF registers are: %3d %3d\n", pca9685::query_reg(PWM15_OFF_H) & 0xFF, pca9685::query_reg(PWM15_OFF_L) & 0xFF);
-    printf("ON registers are: %3d %3d\n", pca9685::query_reg(PWM15_ON_H) & 0xFF, pca9685::query_reg(PWM15_ON_L) & 0xFF);
+    // printf("OFF registers are: %3d %3d\n", pca9685::query_reg(PWM15_OFF_H) & 0xFF, pca9685::query_reg(PWM15_OFF_L) & 0xFF);
+    // printf("ON registers are: %3d %3d\n", pca9685::query_reg(PWM15_ON_H) & 0xFF, pca9685::query_reg(PWM15_ON_L) & 0xFF);
 
     usleep(5000000);
 
